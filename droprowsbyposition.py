59a1c13ae5c04641ec829b42980768e84945ad55
alexgonca <alexandre.reportagem@gmail.com>
2017-09-13 13:47:38 -0400

    Initialize repository

---
+++ droprowsbyposition.py
@@ -0,0 +1,22 @@
+class Importable:
+    @staticmethod
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def event():
+        pass
+
+    @staticmethod
+    def render(wf_module, table):
+        first_row = wf_module.get_param_number('first_row')
+        last_row = wf_module.get_param_number('last_row')
+        if first_row != None and last_row != None:
+            # index is zero-based
+            first_row = first_row - 1
+            last_row = last_row - 1
+            newtab = table.ix[first_row:last_row]
+            wf_module.set_ready(notify=False)
+            return newtab
+        else:
+            return table
