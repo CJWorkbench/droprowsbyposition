4ed481361557cd8596f94653bbbf1bb2fb399888
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 14:40:19 -0800

    Update to new API

--- droprowsbyposition.py
+++ droprowsbyposition.py
@@ -1,28 +1,16 @@
-import pandas as pd
-
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-        first_row = wf_module.get_param_integer('first_row')
-        last_row = wf_module.get_param_integer('last_row')
-        if first_row >= 0 and last_row > 0:
-            # index is zero-based
-            # we don't need to decrement last_row because iloc does not include the upper bound
-            if first_row != 0:
-                first_row = first_row - 1
-            if first_row == 0:
-                newtab = table.iloc[last_row:]
-            else:
-                newtab = pd.concat([table.iloc[0:first_row], table.iloc[last_row:]])
-            wf_module.set_ready(notify=False)
-            return newtab
+def render(table, params):
+    first_row = params['first_row']
+    last_row = params['last_row']
+    if first_row >= 0 and last_row > 0:
+        # index is zero-based
+        # we don't need to decrement last_row because iloc does not include the upper bound
+        if first_row != 0:
+            first_row = first_row - 1
+        if first_row == 0:
+            newtab = table.iloc[last_row:]
         else:
-            return table
+            newtab = pd.concat([table.iloc[0:first_row], table.iloc[last_row:]])
+        return newtab
+    else:
+        return table
+    
