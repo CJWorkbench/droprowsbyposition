4a7ce87db34e927ad6196d3ce7bcc19484982b48
Adam Hooper <adam@adamhooper.com>
2018-09-05 15:23:26 -0400

    PEP8-ize

--- droprowsbyposition.py
+++ droprowsbyposition.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
+
 def render(table, params):
     first_row = params['first_row']
     last_row = params['last_row']
     if first_row >= 0 and last_row > 0:
         # index is zero-based
-        # we don't need to decrement last_row because iloc does not include the upper bound
+        # we don't need to decrement last_row because iloc does not include the
+        # upper bound
         if first_row != 0:
             first_row = first_row - 1
         if first_row == 0:
             newtab = table.iloc[last_row:]
         else:
-            newtab = pd.concat([table.iloc[0:first_row], table.iloc[last_row:]])
+            newtab = pd.concat([table.iloc[0:first_row],
+                                table.iloc[last_row:]])
         return newtab
     else:
         return table
-    
