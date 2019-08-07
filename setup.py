49dc5e8d2b62c08e2f93654984e695abfa79ff3a
Adam Hooper <adam@adamhooper.com>
2019-08-07 14:05:13 -0400

    Test with Pandas 0.25.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='droprowsbyposition',
-    version='0.0.2',
-    description='Drop specified rows',
-    author='Adam Hooper',
-    author_email='adam@adamhooper.com',
-    url='https://github.com/CJWorkbench/droprowsbyposition',
-    packages=[''],
-    py_modules=['droprowsbyposition'],
-    install_requires=['pandas==0.24.2']
+    name="droprowsbyposition",
+    version="0.0.2",
+    description="Drop specified rows",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/droprowsbyposition",
+    packages=[""],
+    py_modules=["droprowsbyposition"],
+    install_requires=["pandas==0.25.0"],
 )
